3177dcee0d6ca695fa2560a4ef89090e135e4b12
JoCode-dev <cedricduakon@gmail.com>
2025-04-21 15:20:19 +0000

    feat: :sparkles: Enhance LabCanvas with file preview and removal functionality

--- src/components/sections/lab/lab-canvas.tsx
+++ src/components/sections/lab/lab-canvas.tsx
@@ -4,7 +4,9 @@ import { cn } from "@/lib/utils";
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { useDropzone } from "react-dropzone";
-import { UploadIcon } from "lucide-react";
+import { UploadIcon, XIcon } from "lucide-react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 const mainVariant = {
   initial: {
@@ -55,83 +57,65 @@ export const LabCanvas = ({
 
   return (
     <div className="col-span-2 w-full h-full flex flex-col items-center justify-center pl-3">
-      <div
-        className="w-full h-[30rem] flex items-center justify-center border-2 border-dashed rounded-xl"
-        {...getRootProps()}
-      >
-        <motion.div
-          onClick={handleClick}
-          whileHover="animate"
-          className="p-10 group/file block rounded-lg cursor-pointer w-full relative overflow-hidden"
+      {files.length > 0 ? (
+        files.map((file, idx) => (
+          <motion.div
+            key={"file" + idx}
+            layoutId={idx === 0 ? "file-upload" : "file-upload-" + idx}
+            className={cn(
+              "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-center justify-center h-full p-4 mt-4 w-full mx-auto rounded-md",
+              "shadow-sm"
+            )}
+          >
+            <Image
+              src={URL.createObjectURL(file)}
+              alt={file.name}
+              fill
+              className="object-contain"
+            />
+
+            {/* Close button */}
+            <Button
+              variant="ghost"
+              size="icon"
+              onClick={() => {
+                setFiles(files.filter((_, i) => i !== idx));
+              }}
+              className="absolute top-2 right-2 bg-red-500 cursor-pointer"
+            >
+              <XIcon className="size-4" />
+            </Button>
+          </motion.div>
+        ))
+      ) : (
+        <div
+          className="w-full h-[30rem] flex items-center justify-center border-2 border-dashed rounded-xl"
+          {...getRootProps()}
         >
-          <input
-            ref={fileInputRef}
-            id="file-upload-handle"
-            type="file"
-            onChange={(e) => handleFileChange(Array.from(e.target.files || []))}
-            className="hidden"
-          />
-          <div className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,white,transparent)]">
-            <GridPattern />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <p className="relative z-20 font-sans font-bold text-neutral-700 dark:text-neutral-300 text-base">
-              Upload file
-            </p>
-            <p className="relative z-20 font-sans font-normal text-neutral-400 dark:text-neutral-400 text-base mt-2">
-              Drag or drop your files here or click to upload
-            </p>
-            <div className="relative w-full mt-10 max-w-xl mx-auto">
-              {files.length > 0 &&
-                files.map((file, idx) => (
-                  <motion.div
-                    key={"file" + idx}
-                    layoutId={idx === 0 ? "file-upload" : "file-upload-" + idx}
-                    className={cn(
-                      "relative overflow-hidden z-40 bg-white dark:bg-neutral-900 flex flex-col items-start justify-start md:h-24 p-4 mt-4 w-full mx-auto rounded-md",
-                      "shadow-sm"
-                    )}
-                  >
-                    <div className="flex justify-between w-full items-center gap-4">
-                      <motion.p
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        layout
-                        className="text-base text-neutral-700 dark:text-neutral-300 truncate max-w-xs"
-                      >
-                        {file.name}
-                      </motion.p>
-                      <motion.p
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        layout
-                        className="rounded-lg px-2 py-1 w-fit shrink-0 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-white shadow-input"
-                      >
-                        {(file.size / (1024 * 1024)).toFixed(2)} MB
-                      </motion.p>
-                    </div>
-
-                    <div className="flex text-sm md:flex-row flex-col items-start md:items-center w-full mt-2 justify-between text-neutral-600 dark:text-neutral-400">
-                      <motion.p
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        layout
-                        className="px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 "
-                      >
-                        {file.type}
-                      </motion.p>
-
-                      <motion.p
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        layout
-                      >
-                        modified{" "}
-                        {new Date(file.lastModified).toLocaleDateString()}
-                      </motion.p>
-                    </div>
-                  </motion.div>
-                ))}
+          <motion.div
+            onClick={handleClick}
+            whileHover="animate"
+            className="p-10 group/file block rounded-lg cursor-pointer w-full relative overflow-hidden"
+          >
+            <input
+              ref={fileInputRef}
+              id="file-upload-handle"
+              type="file"
+              onChange={(e) =>
+                handleFileChange(Array.from(e.target.files || []))
+              }
+              className="hidden"
+            />
+            <div className="absolute inset-0 [mask-image:radial-gradient(ellipse_at_center,white,transparent)]">
+              <GridPattern />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <p className="relative z-20 font-sans font-bold text-neutral-700 dark:text-neutral-300 text-base">
+                Upload file
+              </p>
+              <p className="relative z-20 font-sans font-normal text-neutral-400 dark:text-neutral-400 text-base mt-2">
+                Drag or drop your files here or click to upload
+              </p>
               {!files.length && (
                 <motion.div
                   layoutId="file-upload"
@@ -164,13 +148,13 @@ export const LabCanvas = ({
               {!files.length && (
                 <motion.div
                   variants={secondaryVariant}
-                  className="absolute opacity-0 border border-dashed border-sky-400 inset-0 z-30 bg-transparent flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md"
+                  className="absolute opacity-0 inset-0 z-30 bg-transparent flex items-center justify-center h-32 mt-4 w-full max-w-[8rem] mx-auto rounded-md"
                 ></motion.div>
               )}
             </div>
-          </div>
-        </motion.div>
-      </div>
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 };
